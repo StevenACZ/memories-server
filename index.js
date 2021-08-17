a6c7d5047c223b43de6955563db764cddfce8aa5
stevenacz <scoailazaa@gmail.com>
2021-08-17 10:02:17 -0500

    routes after cors

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import postsRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postsRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postsRoutes);
 
 const CONNECTION_URL =
 	'mongodb+srv://memories:memories@testcluster1.ikdmq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
