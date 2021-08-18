07f0db419bb8c387901f99c5c669c6f67d796708
stevenacz <scoailazaa@gmail.com>
2021-08-18 11:08:17 -0500

    database name changed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/posts', postsRoutes);
 
 const CONNECTION_URL =
-	'mongodb+srv://memories:memories@testcluster1.ikdmq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+	'mongodb+srv://memories:memories@testcluster1.ikdmq.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
